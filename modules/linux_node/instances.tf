8a02b5fafca1d0e7ffc45cf02906f7c5345f2278
Varun Sharma <varun.sharma@progress.com>
2021-08-01 19:16:14 +0530

    added code to auto license accept in knife command

--- modules/linux_node/instances.tf
+++ modules/linux_node/instances.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my_vm" {
   tags = var.tags
 
   provisioner "local-exec" {
-    command = "sleep 90; knife bootstrap ${self.public_ip} -U centos -i ~/varun-chef-key.pem --sudo -N ${self.public_ip} --policy-name ${var.chef_policy_name} --policy-group staging -c ~/.chef/config.rb --ssh-verify-host-key=never"
+    command = "sleep 90; knife bootstrap ${self.public_ip} -U centos -i ~/varun-chef-key.pem --sudo -N ${self.public_ip} --policy-name ${var.chef_policy_name} --policy-group staging -c ~/.chef/config.rb --ssh-verify-host-key=never --chef-license accept"
   }
 
   provisioner "remote-exec" {
@@ -26,13 +26,13 @@ resource "aws_instance" "my_vm" {
 
  provisioner "local-exec" {
   when = "destroy"
-  command = "knife node delete -y ${self.public_ip} -c ~/.chef/config.rb"
+  command = "knife node delete -y ${self.public_ip} -c ~/.chef/config.rb --chef-license accept"
   on_failure = continue
  }
 
  provisioner "local-exec" {
   when = "destroy"
-  command = "knife client delete -y ${self.public_ip} -c ~/.chef/config.rb"
+  command = "knife client delete -y ${self.public_ip} -c ~/.chef/config.rb --chef-license accept"
   on_failure = continue
  }
 }
